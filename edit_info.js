0db698e5fbfabbf6b841f5f62e349d57f9c97b3e
HansJebeH <150796054+HansJebeH@users.noreply.github.com>
2024-03-20 17:02:26 +0700

    Add files via upload

---
+++ edit_info.js
@@ -0,0 +1,46 @@
+const { modul } = require('./module');
+const { axios, baileys, chalk, cheerio, child_process, crypto, fs, ffmpeg, jsobfus, moment, ms, speed, util } = modul;
+
+//INFO OWNER
+global.namaowner = `𝐌𝐓𝐒𝟒𝐘𝐎𝐔`// nama owner
+global.creator1 = ["628"]//No Owner 1
+global.creator2 = ["628"]//No Owner 2
+
+//STICKER
+global.packname = `ayam`// nama pack sticker
+global.author = `kokok`// nama author stiker
+
+//KEBUTUHAN CREATE PANEL BOT
+global.domain = 'https://xxxxx.com' // Isi Domain Lu
+global.apikey = 'ptla_0xxxxxxxxxxxi' // isi Apikey Plta Lu
+global.capikey = 'ptlc_ExxxxxxxxxxxS' // isi Apikey Pltc Lu
+global.eggsnya = '15' // id eggs yang dipakai
+global.location = '1' // id location
+
+//CPU PANEL BOT
+global.cpu1 = "30"//%
+global.cpu2 = "50"//%
+global.cpu3 = "70"//%
+global.cpu4 = "90"//%
+global.cpu5 = "110"//%
+
+//RAM PANEL BOT
+global.ram1 = "1050"//GB
+global.ram2 = "2050"//GB
+global.ram3 = "3050"//GB
+global.ram4 = "4050"//GB
+global.ram5 = "5050"//GB
+
+
+
+
+
+
+
+
+
+
+
+
+
+
